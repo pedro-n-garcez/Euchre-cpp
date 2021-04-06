5ee0b0933a771e00d3cdeb31e63bde1f824db135
Pedro <pedro.garcez@loop.colum.edu>
2021-04-06 04:29:51 -0500

    added unit tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,31 @@
 #include <gtest/gtest.h>
 #include "../src/Game.hpp"
 
-int main()
+TEST(CardRankSuitTest, NotFound) {
+    Card * card = new Card(None,A);
+    EXPECT_EQ("Suit not found",card->ReturnSuitString());
+    EXPECT_EQ("Ace",card->ReturnRankString());
+}
+
+TEST(HandWeightTest, LessThanOne){
+    Game game;
+    for (size_t i = 0; i < game.players.size(); i++){
+        ASSERT_LE(game.players[i]->handWeight(Diamonds), 1);
+        ASSERT_LE(game.players[i]->handWeight(Spades), 1);
+        ASSERT_LE(game.players[i]->handWeight(Hearts), 1);
+        ASSERT_LE(game.players[i]->handWeight(Clubs), 1);
+    }
+}
+
+TEST(WantTrumpTest, MinimumWeight){
+    Player * player = new Player(1);
+    ASSERT_TRUE(player->wantTrump(0.66f));
+    ASSERT_TRUE(player->wantTrump(0.65f));
+    ASSERT_FALSE(player->wantTrump(0.64f));
+}
+
+int main(int argc, char **argv)
 {   
-    return 0;
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
